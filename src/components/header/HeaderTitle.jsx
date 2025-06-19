4483ab9bdc182c63add6fe833ddd8e62b19c8234
wbattistetti <107467539+wbattistetti@users.noreply.github.com>
2025-06-19 10:01:59 +0200

    Fix: uso percorso diretto per immagine logo invece di import ES6

--- src/components/header/HeaderTitle.jsx
+++ src/components/header/HeaderTitle.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from '/images/logo.png';
 import './HeaderTitle.css';
 
 const HeaderTitle = () => (
   <div className="header-title-fixed">
     <span className="title-text-fixed">Deseo</span>
-    <img src={logo} alt="Logo" className="header-logo-fixed" />
+    <img src="/images/logo.png" alt="Logo" className="header-logo-fixed" />
     <span className="title-text-fixed">Oculto</span>
   </div>
 );
